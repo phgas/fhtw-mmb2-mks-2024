0e075c55f12eaac26a330ba61bbffb1dd6aa3118
phgas <contact@phgas.dev>
2024-06-27 20:24:39 +0200

    corrected equations

--- euler_1-step_procedure_Gruppe_1A.py
+++ euler_1-step_procedure_Gruppe_1A.py
@@ -23,11 +23,13 @@ def run_simulation():
 
         x[i + 1] = x_dot[i] * STEP_TIME + x[i]
         # is it even correct to only use alpha_dot in second place??
-        x_dot[i + 1] = (x[i] * (alpha_dot[i]**2) - ((C / M_GONDEL) * (x[i] + DELTA_L)) - G * np.sin(PHI_0_RAD) * np.sin(alpha[i]))* STEP_TIME + x_dot[i]
+        x_dot[i + 1] = (x[i] * (alpha_dot[i]**2) - ((C / M_GONDEL) * (x[i] + DELTA_L)) + G * np.sin(PHI_0_RAD) * np.cos(alpha[i]))* STEP_TIME + x_dot[i]
         
+        # is it even correct to only use alpha_dot and x_dot in second place??
         alpha[i + 1] = alpha_dot[i] * STEP_TIME + alpha[i]
-        alpha_dot[i + 1] = (-(M_GONDEL * G * np.sin(PHI_0_RAD) * np.cos(alpha[i]) * x[i]) / 
-                             (M_GONDEL * x[i]**2 + I_STANGE + 20*I_GONDEL)) * STEP_TIME + alpha_dot[i]
+
+        alpha_dot[i + 1] = (-(2 * alpha_dot[i] * x_dot[i] + G * np.sin(PHI_0_RAD) * np.sin(alpha[i]) * x[i]) / 
+                             (x[i]**2 + (5/3) * (B_GONDEL**2 + H_GONDEL**2) + 20 * R_KARUSELL**2)) * STEP_TIME + alpha_dot[i]
 
         time_array[i + 1] = (i + 1) * STEP_TIME
 
@@ -47,20 +49,21 @@ if __name__ == "__main__":
     G = 9.81                               # Gravity acceleration [m/s^2]
     PHI_0_DEG = 30                         # Angle of tilted carousel [°]
     PHI_0_RAD = math.radians(PHI_0_DEG)    # Angle of tilted carousel [radians]
-    C = 107.22                             # Spring stiffness [N/m]
+    R_KARUSELL = 6                         # Radius of carousel [m]
+    C = 888.37                             # Spring stiffness [N/m]
     M_GONDEL = 300                         # Mass of gondola [kg]
     N_GONDEL = 0.2333                      # Mass of gondola [radians/second]
-    DELTA_L = 0.005                        #  [m]
-    I_STANGE = 500                         # Moment of inertia of rod [kg*m^2]
-    I_GONDEL = 218250                      # Moment of inertia of gondola [kg*m^2]
+    B_GONDEL = 1.5                         # Width of gondola [m]
+    H_GONDEL = 1.5                         # Height of gondola [m]
+    DELTA_L = 0.005                        # Max length of locking mechanism [m]
 
     SIMULATION_TIME = 20  # [s]
     STEP_TIME = 0.001     # [s]
     number_of_steps, time_array, x, x_dot, alpha, alpha_dot = setup_simulation(
         SIMULATION_TIME, STEP_TIME)
-    setup_inital_conditions(initial_x=6,                 # Initial displacement (6 meter)
-                            initial_x_dot=0,              # Initial velocity (0 degrees, horizontal position)
-                            initial_alpha=0,             # Initial angular displacement (at rest)
-                            initial_alpha_dot=2*np.pi*N_GONDEL)   # Initial angular velocity (90 degrees, horizontal position)
+    setup_inital_conditions(initial_x=R_KARUSELL,                # Initial displacement (6 meter)
+                            initial_x_dot=0,                     # Initial velocity (0 degrees, horizontal position)
+                            initial_alpha=0,                     # Initial angular displacement (at rest)
+                            initial_alpha_dot=2*np.pi*N_GONDEL)  # Initial angular velocity (90 degrees, horizontal position)
     run_simulation()
     plot_results()
